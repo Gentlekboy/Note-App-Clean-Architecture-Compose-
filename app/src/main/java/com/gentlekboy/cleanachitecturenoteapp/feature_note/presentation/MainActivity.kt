b7ec10cf7e7a2e4c45efea3df19a00433003d9d3
gentlekboy <gentlekboy@gmail.com>
2022-04-02 21:13:06 +0100

    used theme in main activity

--- app/src/main/java/com/gentlekboy/cleanachitecturenoteapp/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/gentlekboy/cleanachitecturenoteapp/feature_note/presentation/MainActivity.kt
@@ -3,13 +3,13 @@ package com.gentlekboy.cleanachitecturenoteapp.feature_note.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.gentlekboy.cleanachitecturenoteapp.ui.theme.CleanAchitectureNoteAppTheme
+import com.gentlekboy.cleanachitecturenoteapp.ui.theme.CleanArchitectureNoteAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CleanAchitectureNoteAppTheme {
+            CleanArchitectureNoteAppTheme {
 
             }
         }
