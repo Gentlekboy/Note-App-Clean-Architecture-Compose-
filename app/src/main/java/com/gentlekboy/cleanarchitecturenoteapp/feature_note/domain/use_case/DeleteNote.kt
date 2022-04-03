e3fbec0dacd41e68d1d0ab6b2e2213e7248bfa33
gentlekboy <gentlekboy@gmail.com>
2022-04-03 19:59:38 +0100

    use case class delete a note

---
+++ app/src/main/java/com/gentlekboy/cleanarchitecturenoteapp/feature_note/domain/use_case/DeleteNote.kt
@@ -0,0 +1,8 @@
+package com.gentlekboy.cleanarchitecturenoteapp.feature_note.domain.use_case
+
+import com.gentlekboy.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.gentlekboy.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+
+class DeleteNote(private val repository: NoteRepository) {
+    suspend operator fun invoke(note: Note) = repository.deleteNote(note)
+}
