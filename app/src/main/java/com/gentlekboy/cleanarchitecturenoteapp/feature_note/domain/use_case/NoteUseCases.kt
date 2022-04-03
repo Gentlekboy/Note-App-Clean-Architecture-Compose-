012430d5b3eb837a4ed4834d5a66fa35d8aff1a6
gentlekboy <gentlekboy@gmail.com>
2022-04-03 20:08:11 +0100

    added documentation

--- app/src/main/java/com/gentlekboy/cleanarchitecturenoteapp/feature_note/domain/use_case/NoteUseCases.kt
+++ app/src/main/java/com/gentlekboy/cleanarchitecturenoteapp/feature_note/domain/use_case/NoteUseCases.kt
@@ -1,5 +1,8 @@
 package com.gentlekboy.cleanarchitecturenoteapp.feature_note.domain.use_case
 
+/**
+ * Wrapper class for injecting [GetNotes] and [DeleteNote] use cases
+ */
 data class NoteUseCases(
     val getNotes: GetNotes,
     val deleteNote: DeleteNote
