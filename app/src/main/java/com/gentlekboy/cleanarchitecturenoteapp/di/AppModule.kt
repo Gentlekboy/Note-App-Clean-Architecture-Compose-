5f82de84a8f9c98934186cbab6e2dc16f10d1caf
gentlekboy <gentlekboy@gmail.com>
2022-04-05 10:58:58 +0100

    added the add note use case for injection

--- app/src/main/java/com/gentlekboy/cleanarchitecturenoteapp/di/AppModule.kt
+++ app/src/main/java/com/gentlekboy/cleanarchitecturenoteapp/di/AppModule.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import com.gentlekboy.cleanarchitecturenoteapp.feature_note.data.data_source.NoteDatabase
 import com.gentlekboy.cleanarchitecturenoteapp.feature_note.data.repository.NoteRepositoryImpl
 import com.gentlekboy.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+import com.gentlekboy.cleanarchitecturenoteapp.feature_note.domain.use_case.AddNote
 import com.gentlekboy.cleanarchitecturenoteapp.feature_note.domain.use_case.DeleteNote
 import com.gentlekboy.cleanarchitecturenoteapp.feature_note.domain.use_case.GetNotes
 import com.gentlekboy.cleanarchitecturenoteapp.feature_note.domain.use_case.NoteUseCases
@@ -34,6 +35,7 @@ object AppModule {
     fun provideNoteUseCases(noteRepository: NoteRepository): NoteUseCases =
         NoteUseCases(
             getNotes = GetNotes(noteRepository),
-            deleteNote = DeleteNote(noteRepository)
+            deleteNote = DeleteNote(noteRepository),
+            addNote = AddNote(noteRepository)
         )
 }
