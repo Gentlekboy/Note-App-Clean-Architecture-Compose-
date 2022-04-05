66df52df03b9c0cba12ebe9e146f7d8f496309d5
gentlekboy <gentlekboy@gmail.com>
2022-04-05 11:23:40 +0100

    created a view model to send state based on users' interaction with the UI

---
+++ app/src/main/java/com/gentlekboy/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesViewModel.kt
@@ -0,0 +1,76 @@
+package com.gentlekboy.cleanarchitecturenoteapp.feature_note.presentation.notes
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.gentlekboy.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.gentlekboy.cleanarchitecturenoteapp.feature_note.domain.use_case.NoteUseCases
+import com.gentlekboy.cleanarchitecturenoteapp.feature_note.domain.util.NoteOrder
+import com.gentlekboy.cleanarchitecturenoteapp.feature_note.domain.util.OrderType
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NotesViewModel @Inject constructor(
+    private val noteUseCases: NoteUseCases
+) : ViewModel() {
+    private val _state = mutableStateOf(NotesState())
+    val state: State<NotesState> = _state
+
+    private var recentlyDeletedNote: Note? = null
+    private var getNotesJob: Job? = null
+
+    init {
+        //Initially update the state with a list of notes, ordered by date in descending order
+        getNotes(NoteOrder.Date(OrderType.Descending))
+    }
+
+    /**
+     * This function is triggered when there is an event from the UI
+     */
+    fun onEvent(event: NotesEvent) {
+        when (event) {
+            is NotesEvent.Order -> {
+                if (state.value.noteOrder::class == event.noteOrder::class &&
+                    state.value.noteOrder.orderType == event.noteOrder.orderType
+                ) return
+
+                getNotes(event.noteOrder)
+            }
+            is NotesEvent.DeleteNote -> {
+                viewModelScope.launch {
+                    noteUseCases.deleteNote(event.note)
+                    recentlyDeletedNote = event.note
+                }
+            }
+            is NotesEvent.RestoreNote -> {
+                viewModelScope.launch {
+                    noteUseCases.addNote(recentlyDeletedNote ?: return@launch)
+                    recentlyDeletedNote = null
+                }
+            }
+            is NotesEvent.ToggleOrderSection -> {
+                _state.value =
+                    state.value.copy(isOrderSectionVisible = !state.value.isOrderSectionVisible)
+            }
+        }
+    }
+
+    /**
+     * This function gets all notes form the local database and updates the state with the corresponding data
+     */
+    private fun getNotes(noteOrder: NoteOrder) {
+        getNotesJob?.cancel()
+        getNotesJob = noteUseCases.getNotes(noteOrder).onEach { notes ->
+            _state.value = state.value.copy(
+                notes = notes,
+                noteOrder = noteOrder
+            )
+        }.launchIn(viewModelScope)
+    }
+}
