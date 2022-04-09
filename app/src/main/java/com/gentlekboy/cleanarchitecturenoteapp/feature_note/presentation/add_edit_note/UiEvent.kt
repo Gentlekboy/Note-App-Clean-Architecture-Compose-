b4eb38db4992999ff48bed03595cb820c865728b
gentlekboy <gentlekboy@gmail.com>
2022-04-09 20:53:27 +0100

    created event wrapper for saving notes and showing a snack bar

---
+++ app/src/main/java/com/gentlekboy/cleanarchitecturenoteapp/feature_note/presentation/add_edit_note/UiEvent.kt
@@ -0,0 +1,6 @@
+package com.gentlekboy.cleanarchitecturenoteapp.feature_note.presentation.add_edit_note
+
+sealed class UiEvent {
+    data class ShowSnackBar(val message: String) : UiEvent()
+    object SaveNote : UiEvent()
+}
